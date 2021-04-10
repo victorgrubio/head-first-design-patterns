96e54c6e31fefa4e36b2d8f39b29955bf0585b3e
Victor Garcia <victorgrubiodl@gmail.com>
2021-04-10 21:35:49 +0200

    Pizza per type definition  in main

--- factory/main.py
+++ factory/main.py
@@ -1,7 +1,13 @@
 from pizza import Pizza
 
 def order_pizza(pizza_type: str) -> Pizza:
-    pizza = Pizza()
+    pizza: Pizza = None
+    if pizza_type == "cheese":
+        pizza = CheesePizza()
+    elif pizza_type == "greek":
+        pizza = GreekPizza
+    elif pizza_type == "pepperoni":
+        pizza = PepperoniPizza()
     pizza.prepare()
     pizza.bake()
     pizza.cut()
